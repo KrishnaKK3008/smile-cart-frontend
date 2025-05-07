a4a47434eb9210488e2f584675c2c3af0e0e4d57
KrishnaKK3008 <krishnakumarkhuntia3008@gmail.com>
2025-05-07 17:55:19 +0530

    Completed Chapter 12

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
-import Product from "./components/Product";
+import { PageNotFound } from "components/commons";
+import { Route, Switch, Redirect } from "react-router-dom";
+import routes from "routes";
 
-const App = () => <Product />;
+import "./App.css";
+import Product from "./components/Product";
+import ProductList from "./components/ProductList";
+
+const App = () => (
+  <Switch>
+    <Route exact component={Product} path={routes.products.show} />
+    <Route exact component={ProductList} path={routes.products.index} />
+    <Redirect exact from={routes.root} to={routes.products.index} />
+    <Route component={PageNotFound} path="*" />
+  </Switch>
+);
 export default App;
